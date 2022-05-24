26e83e5f66471668afb69b401387ca1b63e82d34
FogonazoCultural <fogonazocultural@gmail.com>
2022-05-24 13:32:41 -0400

    Mejore la identacion

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { useState, useEffect } from 'react';
-import { Heading } from '@chakra-ui/react'
-import { LaunchItem } from './components/LaunchItem';
-import * as API from './services/launches';
-
+import { useState, useEffect } from "react";
+import { Heading } from "@chakra-ui/react";
+import { LaunchItem } from "./components/LaunchItem";
+import * as API from "./services/launches";
 
 function App() {
   const [launches, setLaunches] = useState([]);
@@ -10,13 +9,15 @@ function App() {
   useEffect(() => {
     API.getAllLaunches().then(setLaunches);
   }, []);
-  
+
   return (
     <>
-      <Heading as='h1' size='lg' m={4}>SpaceX Launches</Heading>
+      <Heading as="h1" size="lg" m={4}>
+        SpaceX Launches
+      </Heading>
       <section>
         {launches.map((launch) => (
-          <LaunchItem key={launch.flight_number} {...launch}/>
+          <LaunchItem key={launch.flight_number} {...launch} />
         ))}
       </section>
     </>
